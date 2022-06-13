a8b0f6bd351f2aaf28499da86197ec528ca4b664
dinhdn@rikkeisoft <dinhdn@rikkeisoft.com>
2022-06-13 16:59:49 +0700

    add library handlebars - template engine

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
+app.use(morgan('combined'));
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('<h1 style="color:red">Hello world!</h1>');
 });
 
 app.listen(port, () => {
